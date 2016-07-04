130445a870b1cd064f48d39f319e915912d37277
Ben Greene <b.greene@analyticsengines.com>
2016-07-05 00:22:51 +0100

    Test out fixture to manage session and connection

--- test-2.7/scripts/conftest.py
+++ test-2.7/scripts/conftest.py
@@ -7,27 +7,30 @@ def pytest_addoption(parser):
     parser.addoption("--db", action="store", default="udv", help="db: db to access the DB")
 
 
-# @pytest.fixture
-# def username(request):
-#     return request.config.getoption("--username")
-#
-#
-# @pytest.fixture
-# def password(request):
-#     return request.config.getoption("--password")
-#
-#
-# @pytest.fixture
-# def db(request):
-#     return request.config.getoption("--db")
+@pytest.fixture(scope='module')
+def username(request):
+    return request.config.getoption("--username")
 
 
-@pytest.fixture(scope='class')
-def params(request):
-    request.cls.username = request.config.getoption("--username")
-    request.cls.password = request.config.getoption("--password")
-    request.cls.db = request.config.getoption("--db")
+@pytest.fixture(scope='module')
+def password(request):
+    return request.config.getoption("--password")
+
+
+@pytest.fixture(scope='module')
+def db(request):
+    return request.config.getoption("--db")
+
+
+# @pytest.fixture(scope='class')
+# def params(request):
+#     request.cls.username = request.config.getoption("--username")
+#     request.cls.password = request.config.getoption("--password")
+#     request.cls.db = request.config.getoption("--db")
 
 
 def pytest_cmdline_preparse(args):
-    args[:] = ["-k", "\"not UdvBaseTest\""] + args
+    """
+    Do not run built in tests from the test framework against the framework itself
+    """
+    args[:] = ["-k", "\"not MulticornBaseTest\""] + args
